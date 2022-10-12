b3b89fbccc27018e1ea06d05f089536939dbe765
xytki <skomorosi@gmail.com>
2022-10-12 10:06:26 +0200

    Correct app file

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
 from sqlalchemy.orm import query
 
-from schemas import MovieSchema
+from schemas import MovieSchema, GenreSchema, DirectorSchema
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -14,7 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['RESTX_JSON'] = {'ensure_ascii': False}
 db = SQLAlchemy(app)
 api = Api(app)
-
 
 movie_ns = api.namespace("movies")
 movie_schemas = MovieSchema(many=True)
@@ -91,6 +90,115 @@ class MovieViews(Resource):
         data = request.json
         try:
             db.session.query.filter(Movie.id == bid).update(
+                data
+            )
+            db.session.commit()
+            return '', 201
+
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return e, 404
+
+
+genre_ns = api.namespace("genres")
+genre_schemas = GenreSchema(many=True)
+genre_schema = GenreSchema()
+
+
+@genre_ns.route("/")
+class GenreViews(Resource):
+    def get(self):
+        all_genres = Genre.query.all()
+
+        return genre_schemas.dump(all_genres), 200
+
+    def post(self):
+        req_json = request.json
+        new_info = Genre(**req_json)
+
+        with db.session.begin():
+            db.session.add(new_info)
+
+        return '', 201
+
+    def delete(self, bid: int):
+        genre_ = db.session.query(Genre).get(bid)
+
+        db.session.delete(genre_)
+        db.session.commit()
+
+        return '', 204
+
+
+@genre_ns.route("/<int:bid>")
+class GenreViews(Resource):
+    def get(self, bid: int):
+        try:
+            genre_ = Genre.query.get(bid)
+            return movie_schema.dump(genre_), 200
+
+        except Exception as e:
+            return '', 404
+
+    def put(self, bid: int):
+        data = request.json
+        try:
+            db.session.query.filter(Genre.id == bid).update(
+                data
+            )
+            db.session.commit()
+            return '', 201
+
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return e, 404
+
+
+directors_ns = api.namespace("directors")
+director_schemas = DirectorSchema(many=True)
+director_schema = DirectorSchema()
+
+
+@directors_ns.route("/")
+class DirectorsViews(Resource):
+    def get(self):
+        all_directors = Director.query.all()
+
+        return director_schemas.dump(all_directors), 200
+
+    def post(self):
+        req_json = request.json
+        new_info = Director(**req_json)
+
+        with db.session.begin():
+            db.session.add(new_info)
+
+        return '', 201
+
+    def delete(self, bid: int):
+        genre_ = db.session.query(Director).get(bid)
+
+        db.session.delete(genre_)
+        db.session.commit()
+
+        return '', 204
+
+
+@directors_ns.route("/<int:bid>")
+class DirectorsViews(Resource):
+    def get(self, bid: int):
+        try:
+            genre_ = Director.query.get(bid)
+            return movie_schema.dump(genre_), 200
+        except Exception as e:
+            return '', 404
+
+    def put(self, bid: int):
+        data = request.json
+        try:
+            db.session.query.filter(Director.id == bid).update(
                 data
             )
             db.session.commit()
